5b996ba06c125a76fcf4e9431b1b13fffc1c4ce0
923433-hash <923433@pdsb.net>
2025-12-18 09:42:29 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,77 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . b . b b b b b . . . . . 
+        . . . . . b f f f f f b . . . . 
+        . . . . b . b b b b b . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, spaceplane, 50, 50)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.fire, 500)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    scene.cameraShake(4, 500)
+    info.changeLifeBy(-1)
+})
+let projectile: Sprite = null
+let bogey: Sprite = null
+let spaceplane: Sprite = null
+spaceplane = sprites.create(img`
+    ..ccc.........ffffff....
+    ..f4cc.......fcc22ff....
+    ..f44cc...fffccccff.....
+    ..f244cccc22224442cc....
+    ..f224cc2222222244b9c...
+    ..cf2222222222222b999c..
+    .c22c222222222b11199b2c.
+    f22ccccccc222299111b222c
+    fffffcc222c222222222222f
+    .....f2222442222222222f.
+    ....f222244fc2222222ff..
+    ...c222244ffffffffff....
+    ...c2222cfffc2f.........
+    ...ffffffff2ccf.........
+    .......ffff2cf..........
+    ........fffff...........
+    `, SpriteKind.Player)
+controller.moveSprite(bogey, 200, 0)
+spaceplane.setStayInScreen(true)
+info.setLife(3)
+controller.moveSprite(spaceplane, 100, 100)
+game.onUpdateInterval(1000, function () {
+    bogey = sprites.create(img`
+        . . . . . . . . c c c c . . . . 
+        . . . . c c c c c c c c c . . . 
+        . . . c f c c a a a a c a c . . 
+        . . c c f f f f a a a c a a c . 
+        . . c c a f f c a a f f f a a c 
+        . . c c a a a a b c f f f a a c 
+        . c c c c a c c b a f c a a c c 
+        c a f f c c c a b b 6 b b b c c 
+        c a f f f f c c c 6 b b b a a c 
+        c a a c f f c a 6 6 b b b a a c 
+        c c b a a a a b 6 b b a b b a . 
+        . c c b b b b b b b a c c b a . 
+        . . c c c b c c c b a a b c . . 
+        . . . . c b a c c b b b c . . . 
+        . . . . c b b a a 6 b c . . . . 
+        . . . . . . b 6 6 c c . . . . . 
+        `, SpriteKind.Enemy)
+    bogey.setVelocity(-100, 0)
+    bogey.setPosition(160, randint(115, 10))
+    bogey.setFlag(SpriteFlag.AutoDestroy, true)
+})
